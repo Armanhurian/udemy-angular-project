83673df7d9b8ad3e976dae33ea76f15e46c29d4b
Arman <hurianarmann@gmail.com>
2023-10-13 00:12:02 +0330

    10 Day Of Udemy Practice

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,16 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   
   constructor( private http : HttpClient ){}
-  
+
+  isUpload : boolean = false
+
+  imageSrc : string = ''
+
+  nameOfUser : string = ''
+
+  emailOfUser : string = ''
+
+  birthDay : string = ''
   
   getAnimals(){
 
@@ -17,7 +26,7 @@ export class HomeComponent {
     searchMyParams = searchMyParams.append('type' , 'fast').append('options', 'fullStack')
     //searchMyParams = searchMyParams.append('options' , 'fullStack')
 
-    this.http.get('http://api.agromonitoring.com/agro/1.0/polygons/incorrect',
+    this.http.get('https://randomuser.me/api/',
     
     {
 
@@ -27,8 +36,19 @@ export class HomeComponent {
     }
 
     ).subscribe((data)=>{
-      console.log(data); 
+
+      this.isUpload = true
+   
+      this.imageSrc = Object.values(data)[0][0]["picture"]["large"]
+      this.nameOfUser = Object.values(data)[0][0]["name"]["first"]
+      this.emailOfUser = Object.values(data)[0][0]["email"]
+      this.birthDay = Object.values(data)[0][0]["dob"]["date"].slice(0,10)
+
     })
+  }
+
+  removeUser(){
+    this.isUpload = false
   }
 
 }
